b157d44efcf9cc9c67375bad5e587111bfd448ac
Sachin Singh <singhsachin9939@gmail.com>
2025-05-28 19:34:16 +0530

    Create basic_python_practice.py

---
+++ basic_python_practice.py
@@ -0,0 +1,20 @@
+# My first Python practice file
+
+# Print a welcome message
+print("Welcome to my Python journey!")
+
+# Declare some variables
+name = "Sachin Singh"
+age = 20
+is_student = True
+
+# Print values
+print("Name:", name)
+print("Age:", age)
+print("Is Student:", is_student)
+
+# Simple calculation
+a = 10
+b = 5
+sum = a + b
+print("Sum of a and b is:", sum)
