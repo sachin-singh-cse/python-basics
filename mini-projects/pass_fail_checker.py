dcb31c4ee69a6a1c876a5f077bd2336e2fe3d211
Sachin Singh <singhsachin9939@gmail.com>
2025-06-05 19:59:09 +0530

    🚀 Added Pass/Fail Checker project (Python)

---
+++ mini-projects/pass_fail_checker.py
@@ -0,0 +1,10 @@
+# Project: Pass and Fail Checker
+# Description: Checks if a student has passed based on marks
+# Author: Sachin Singh
+
+marks = float(input("Enter your marks: "))
+
+if marks >= 33:
+    print("🎉 Congratulations! You passed.")
+else:
+    print("❌ Sorry, you failed.")
