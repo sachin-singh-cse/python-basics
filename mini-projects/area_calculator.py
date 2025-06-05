ee47f1f421d2bb9e5256e9c3409e5de8a0aec9ce
Sachin Singh <singhsachin9939@gmail.com>
2025-06-05 20:05:30 +0530

    Added Area Calculator mini project

---
+++ mini-projects/area_calculator.py
@@ -0,0 +1,9 @@
+# Project: Area Calculator
+# Description: Calculates the area of a circle using radius input
+# Author: Sachin Singh
+
+radius = float(input("Enter the radius of the circle: "))
+pi = 3.14
+
+area = pi * radius ** 2
+print(f"🟢 Area of the circle is: {area}")
