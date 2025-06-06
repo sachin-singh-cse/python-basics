7bd59c4e85184d006df16e08f9fa5400d5af4aa2
Sachin Singh <singhsachin9939@gmail.com>
2025-06-06 06:29:32 +0530

    Added Celsius to Fahrenheit Converter mini project

---
+++ mini-projects/temp_converter.py
@@ -0,0 +1,8 @@
+# Project: Celsius to Fahrenheit Converter
+# Description: Converts temperature from Celsius to Fahrenheit
+# Author: Sachin Singh
+
+celsius = float(input("Enter temperature in °C: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"🌡️ Temperature in Fahrenheit: {fahrenheit}°F")
